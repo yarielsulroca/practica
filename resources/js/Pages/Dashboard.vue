8be0cd3ddce0597962a6c54f550522a01e1660a5
yariel Sulroca <sulroca@gmail.com>
2024-05-29 18:07:48 -0300

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+	<Head title="Dashboard" />
+
+	<AuthenticatedLayout>
+		<template #header>
+			Bienvenido
+		</template>
+
+		<div class="p-4 bg-white rounded-lg shadow-xs">
+			!Sistema Electromecánica GyG !
+		</div>
+	</AuthenticatedLayout>
+</template>
